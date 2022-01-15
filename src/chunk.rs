83129b2f61034c76fbbe3d1e91123929badd62a4
Diego Freijo <giga.freijo@gmail.com>
2022-01-14 22:43:26 -0300

    added constants

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,43 +1,58 @@
-use std::mem;
+use std::{mem, vec};
 
 // use std::fmt::Display;
 
+type Value = f64;
 
 #[derive(Debug)]
 pub enum Operation {
-	Return,
+    Constant(usize),
+    Return,
 }
 
 impl Operation {
-	pub fn disassemble(&self, offset: usize) {
-		println!("{:04}	{:?}", offset, &self);
-	}
+    pub fn disassemble(&self, chunk: &Chunk, offset: usize) {
+        print!("{:04}	", offset);
+        match self {
+            Operation::Constant(constant_offset) => {
+                println!("Constant {}", &chunk.constants[*constant_offset])
+            }
+            Operation::Return => println!("Return"),
+        }
+    }
 }
 
 // #[derive(Debug)]
 pub struct Chunk {
-	code: Vec<Operation>,
+    code: Vec<Operation>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
-	pub fn new() -> Chunk {
-		Chunk {
-			code: vec![],
-		}
-	}
+    pub fn new() -> Chunk {
+        Chunk {
+            code: vec![],
+            constants: vec![],
+        }
+    }
 
-	pub fn write(&mut self, op: Operation) {
-		self.code.push(op);
-	}
+    pub fn write(&mut self, op: Operation) {
+        self.code.push(op);
+    }
 
-	pub fn disassemble(&self, name: &str) {
-		println!("== {} ==\n", name);
-		let mut offset: usize = 0;
-		for op in &self.code {
-			op.disassemble(offset);
-			offset += mem::size_of_val(&op);
-		}
-	}
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
+    }
+
+    pub fn disassemble(&self, name: &str) {
+        println!("== {} ==\n", name);
+        let mut offset: usize = 0;
+        for op in &self.code {
+            op.disassemble(self, offset);
+            offset += mem::size_of_val(&op);
+        }
+    }
 }
 
 // impl Display for Chunk {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use chunk::Chunk;
 
+mod value;
 mod chunk;
 
 fn main() {
     let mut chunk = Chunk::new();
-    chunk.write(chunk::Operation::Return);
-    chunk.write(chunk::Operation::Return);
+    
+    let constant = chunk.add_constant(1.2);
+    chunk.write(chunk::Operation::Constant(constant));
+    
+    let constant = chunk.add_constant(4.5);
+    chunk.write(chunk::Operation::Constant(constant));
+    
     chunk.write(chunk::Operation::Return);
     chunk.disassemble("test chunk");
 }
